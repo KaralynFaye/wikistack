a8f9aa4e266445a8ed1f2ba8ddfb0ea51d651e88
Karalyn Faye <karalyn.ferrari@gmail.com>
2018-06-15 12:43:44 -0400

    funky

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const addPage = require('../views/addPage');
 module.exports = router;
+const bodyParser = require ('body-parser')
+
+//router.use(bodyParser.urlencoded({extended:true}));
+//router.use(bodyParser.json());
 
 router.get('/', (req, res, next) => {
+  res.redirect('/');
 
 });
 
@@ -13,4 +18,5 @@ router.post('/', (req, res, next) => {
 
 router.get('/add', (req, res, next) => {
     res.send(addPage());
+    //res.json(req.body);
 });
